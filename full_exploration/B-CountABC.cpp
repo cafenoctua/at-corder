4cbd6c386e6825e32b3225060cad894437faf7f9
渡辺ブルーノ <watanabeburuno@watanabeburunonoMacBook-Air.local>
2020-08-06 00:06:29 +0900

    Entry full cxploration

---
+++ full_exploration/B-CountABC.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string  s;
+    cin >> s;
+    int cnt = 0;
+
+    for (int i = 0; i < n-2; i++) {
+        if (s.substr(i,3) == "ABC") cnt++;
+    }
+
+    cout << cnt << endl;
+}
